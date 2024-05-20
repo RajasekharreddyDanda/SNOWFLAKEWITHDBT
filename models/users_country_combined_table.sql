bcec71f651b4d115aaff3a05836dea34b49ba89e
danda reddy <dandareddy72@gmail.com>
2024-05-20 12:11:48 +0000

    class4

---
+++ models/users_country_combined_table.sql
@@ -0,0 +1,13 @@
+--select * from {{source("source table's info", "raw_users")}}
+--select * from {{source("source table's info", "raw_country")}}
+
+select
+    ru.id,
+    ru.user,
+    rc.country as countryname
+from
+    {{ source("source table's info", 'raw_users') }} ru
+left join
+    {{ source("source table's info", 'raw_country') }} rc
+on
+    ru.country = rc.country_id
